067682763a8fba654049f8de6a139b40265cdbb5
indigoxela <user@olga.indigofloat.at>
2024-03-03 12:14:42 +0100

    Result of phpcs fixes

--- filefield_paths.api.php
+++ filefield_paths.api.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @file
  * Hooks provided by the File (Field) Paths module.
@@ -9,9 +8,9 @@
  * Define field(s) to be displayed on the File (Field) Paths settings form and
  * used during the processing of uploaded files.
  *
- * @param $field
+ * @param array $field
  *   The field definition this File (Field) Paths settings field applies to.
- * @param $instance
+ * @param array $instance
  *   The field instance this File (Field) Paths settings field applies to.
  *
  * @return array
@@ -20,9 +19,9 @@
  *   - title: The title fo the field.
  *   - form: A keyed array of Form API elements.
  *
- * @see hook_filefield_paths_process_file().
+ * @see hook_filefield_paths_process_file()
  */
-function hook_filefield_paths_field_settings($field, $instance) {
+function hook_filefield_paths_field_settings(array $field, array $instance) {
   return array(
     'file_path' => array(
       'title' => 'File path',
@@ -52,20 +51,20 @@ function hook_filefield_paths_field_type_info() {
 /**
  * Process the uploaded files.
  *
- * @param $type
+ * @param string $type
  *   The entity type containing the files for processing.
- * @param $entity
+ * @param object $entity
  *   The entity containing the files for processing.
- * @param $field
+ * @param array $field
  *   The definition of the field containing the files for processing.
- * @param $instance
+ * @param array $instance
  *   The instance of the field containing the files for processing.
- * @param $langcode
+ * @param string $langcode
  *   The language code of the field containing the files for processing.
- * @param $items
+ * @param array $items
  *   A pass-by-reference array of all the files for processing.
  *
- * @see filefield_paths_filefield_paths_process_file().
+ * @see filefield_paths_filefield_paths_process_file()
  */
-function hook_filefield_paths_process_file($type, $entity, $field, $instance, $langcode, &$items) {
+function hook_filefield_paths_process_file($type, $entity, array $field, array $instance, $langcode, array &$items) {
 }
